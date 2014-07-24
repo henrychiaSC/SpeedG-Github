9a97e51ea74da78991b5a9d5cbbaef5599fca6c9
wackojoy <speedg>
2014-07-24 11:16:58 +0800

    Bar Chart
    Working

--- js/AdminLTE/app.js
+++ js/AdminLTE/app.js
@@ -134,6 +134,76 @@ $(function() {
         radioClass: 'iradio_minimal'
     });
 
+    /* For demo purposes */
+/*    var demo = $("<div />").css({
+        position: "fixed",
+        top: "150px",
+        right: "0",
+        background: "rgba(0, 0, 0, 0.7)",
+        "border-radius": "5px 0px 0px 5px",
+        padding: "10px 15px",
+        "font-size": "16px",
+        "z-index": "999999",
+        cursor: "pointer",
+        color: "#ddd"
+    }).html("<i class='fa fa-gear'></i>").addClass("no-print");*/
+
+  /*  var demo_settings = $("<div />").css({
+        "padding": "10px",
+        position: "fixed",
+        top: "130px",
+        right: "-200px",
+        background: "#fff",
+        border: "3px solid rgba(0, 0, 0, 0.7)",
+        "width": "200px",
+        "z-index": "999999"
+    }).addClass("no-print");*/
+   /* demo_settings.append(
+            "<h4 style='margin: 0 0 5px 0; border-bottom: 1px dashed #ddd; padding-bottom: 3px;'>Layout Options</h4>"
+            + "<div class='form-group no-margin'>"
+            + "<div class='.checkbox'>"
+            + "<label>"
+            + "<input type='checkbox' onchange='change_layout();'/> "
+            + "Fixed layout"
+            + "</label>"
+            + "</div>"
+            + "</div>"
+            );*/
+   /* demo_settings.append(
+            "<h4 style='margin: 0 0 5px 0; border-bottom: 1px dashed #ddd; padding-bottom: 3px;'>Skins</h4>"
+            + "<div class='form-group no-margin'>"
+            + "<div class='.radio'>"
+            + "<label>"
+            + "<input name='skins' type='radio' onchange='change_skin(\"skin-black\");' /> "
+            + "Black"
+            + "</label>"
+            + "</div>"
+            + "</div>"
+
+            + "<div class='form-group no-margin'>"
+            + "<div class='.radio'>"
+            + "<label>"
+            + "<input name='skins' type='radio' onchange='change_skin(\"skin-blue\");' checked='checked'/> "
+            + "Blue"
+            + "</label>"
+            + "</div>"
+            + "</div>"
+            );*/
+
+    /*demo.click(function() {
+        if (!$(this).hasClass("open")) {
+            $(this).css("right", "200px");
+            demo_settings.css("right", "0");
+            $(this).addClass("open");
+        } else {
+            $(this).css("right", "0");
+            demo_settings.css("right", "-200px");
+            $(this).removeClass("open")
+        }
+    });*/
+
+    /*$("body").append(demo);
+    $("body").append(demo_settings);*/
 });
 function fix_sidebar() {
     //Make sure the body tag has the .fixed class
@@ -591,7 +661,7 @@ $(window).load(function() {
             //initialize already active menus
             if (isActive) {
                 menu.show();
-                btn.children(".fa-angle-left").first().removeClass("fa-angle-left").addClass("fa-angle-down");
+                btn.children(".fa-caret-left").first().removeClass("fa-caret-left").addClass("fa-caret-down");
             }
             //Slide open or close the menu on link click
             btn.click(function(e) {
@@ -600,13 +670,13 @@ $(window).load(function() {
                     //Slide up to close menu
                     menu.slideUp();
                     isActive = false;
-                    btn.children(".fa-angle-down").first().removeClass("fa-angle-down").addClass("fa-angle-left");
+                    btn.children(".fa-caret-down").first().removeClass("fa-caret-down").addClass("fa-caret-left");
                     btn.parent("li").removeClass("active");
                 } else {
                     //Slide down to open menu
                     menu.slideDown();
                     isActive = true;
-                    btn.children(".fa-angle-left").first().removeClass("fa-angle-left").addClass("fa-angle-down");
+                    btn.children(".fa-caret-left").first().removeClass("fa-caret-left").addClass("fa-caret-down");
                     btn.parent("li").addClass("active");
                 }
             });
